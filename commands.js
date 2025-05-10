cfd4f97fb2b956dfbb63f1ac785ee5021fd60593
prasantbhusal <132643420+prasantbhusal@users.noreply.github.com>
2025-05-10 23:42:15 +0545

    Add files via upload

---
+++ commands.js
@@ -0,0 +1,10 @@
+Cypress.Commands.add('login', (email, password) => {
+  cy.visit('/login');
+  cy.get('input[name=email]').type(email);
+  cy.get('input[name=password]').type(password);
+  cy.get('button[type=submit]').click();
+});
+
+Cypress.Commands.add('addToCart', (productName) => {
+  cy.contains(productName).parent().contains('Add to cart').click();
+});
